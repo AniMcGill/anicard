10906832223a12e5125f2048192ff4fd54c27288
Michael Williams <draringi@draringi.net>
2015-09-20 22:54:37 -0400

    fix missing ) in urls.py

--- anicard/urls.py
+++ anicard/urls.py
@@ -3,3 +3,4 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'anicard.views.memberCardView', name="card_view"),
     url(r'^request/$', 'anicard.views.cardrequest', name="card_request"),
+)
