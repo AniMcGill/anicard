996f2522223fd8b65c51332341d66c79fe1cff47
Michael Williams <draringi@draringi.net>
2015-09-20 23:25:52 -0400

    fixes the form

--- anicard/forms.py
+++ anicard/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class CardReqForm(forms.Form):
     first_name = forms.CharField(max_length=30, label="Given Name", widget=forms.TextInput(
         attrs={'required': 'required'}))
-    last_name = forms.CharField(max_length=200, widget=forms.PasswordInput(
+    last_name = forms.CharField(max_length=30, widget=forms.TextInput(
         attrs={'required': 'required'}), label="Family Name")
